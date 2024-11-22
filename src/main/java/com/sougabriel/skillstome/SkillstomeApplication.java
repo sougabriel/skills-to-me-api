cd30205a5894209e2b7ae524d2fe24cd1bf117b7
Gabriel de Souza <110489605+sougabriel@users.noreply.github.com>
2024-11-21 21:58:56 -0300

    initial commit

---
+++ src/main/java/com/sougabriel/skillstome/SkillstomeApplication.java
@@ -0,0 +1,13 @@
+package com.sougabriel.skillstome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillstomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillstomeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sougabriel/skillstome/SkillstomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sougabriel.skillstome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillstomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
