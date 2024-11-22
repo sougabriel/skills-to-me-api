00c56bfb0ffa19c5b0b958d7f7273a8d88023be1
sougabriel <gabriel.dsouzapro@gmail.com>
2024-11-22 17:32:35 -0300

    feat(user): create  interface and implementation

--- src/main/java/com/sougabriel/skillstome/service/UserService.java
+++ src/main/java/com/sougabriel/skillstome/service/UserService.java
@@ -1,4 +1,42 @@
 package com.sougabriel.skillstome.service;
 
-public class UserService {
+import com.sougabriel.skillstome.model.UserModel;
+import com.sougabriel.skillstome.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class UserService implements UserServiceInterface {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserModel save(UserModel userModel) {
+        return this.userRepository.save(userModel);
+    }
+
+    @Override
+    public List<UserModel> findAll() {
+        return this.userRepository.findAll();
+    }
+
+    @Override
+    public Optional<UserModel> findById(UUID id) {
+        return this.userRepository.findById(id);
+    }
+
+    @Override
+    public UserModel update(UserModel userModel) {
+        return this.userRepository.save(userModel);
+    }
+
+    @Override
+    public void deleteById(UUID id) {
+        this.userRepository.deleteById(id);
+    }
 }
--- src/main/java/com/sougabriel/skillstome/service/UserServiceInterface.java
+++ src/main/java/com/sougabriel/skillstome/service/UserServiceInterface.java
@@ -1,4 +1,20 @@
 package com.sougabriel.skillstome.service;
 
+import com.sougabriel.skillstome.model.UserModel;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 public interface UserServiceInterface {
+
+    UserModel save(UserModel userModel);
+
+    List<UserModel> findAll();
+
+    Optional<UserModel> findById(UUID id);
+
+    UserModel update(UserModel userModel);
+
+    void deleteById(UUID id);
 }
