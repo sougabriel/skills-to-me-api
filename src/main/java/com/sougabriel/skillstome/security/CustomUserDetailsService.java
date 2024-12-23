be6144d4868a253974b42e19cc0a720d71161976
sougabriel <gabriel.dsouzapro@gmail.com>
2024-12-23 18:00:46 -0300

    feat(security): create `security` package

--- src/main/java/com/sougabriel/skillstome/security/CustomUserDetailsService.java
+++ src/main/java/com/sougabriel/skillstome/security/CustomUserDetailsService.java
@@ -1,4 +1,24 @@
 package com.sougabriel.skillstome.security;
 
-public class CustomUserDetailsService {
+import com.sougabriel.skillstome.user.model.UserModel;
+import com.sougabriel.skillstome.user.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+
+@Component
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserModel user = this.userRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("user not found!"));
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword_hash(), new ArrayList<>());
+    }
 }
--- src/main/java/com/sougabriel/skillstome/security/TokenService.java
+++ src/main/java/com/sougabriel/skillstome/security/TokenService.java
@@ -1,4 +1,51 @@
 package com.sougabriel.skillstome.security;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.sougabriel.skillstome.user.model.UserModel;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
 public class TokenService {
+
+    @Value("${api.security.token.secret}")
+    private String secretKey;
+
+    public String generateToken(UserModel user) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(this.secretKey);
+            return JWT.create()
+                    .withIssuer("skillstome")
+                    .withSubject(user.getEmail())
+                    .withExpiresAt(this.generateExpirationTime())
+                    .sign(algorithm);
+        } catch (JWTCreationException e) {
+            throw new RuntimeException("Error while authenticating" + e);
+        }
+    }
+
+    public String validateToken(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(this.secretKey);
+            return JWT.require(algorithm)
+                    .withIssuer("skillstome")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        } catch (JWTVerificationException e) {
+            return null;
+        }
+    }
+
+    private Instant generateExpirationTime() {
+        return LocalDateTime.now().plusHours(16).toInstant(ZoneOffset.of("-03:00"));
+    }
+
 }
